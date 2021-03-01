5efe6404b704711e578f9664de8a98768aa65a7f
Wily13 <wily.355@gmail.com>
2021-03-01 09:17:10 +0700

    edit file view cart

--- application/views/checkout_snap.php
+++ application/views/checkout_snap.php
@@ -12,7 +12,7 @@
 <body>
 
 
-	<form id="payment-form" method="post" action="https://tokohp.herokuapp.com//snap/finish">
+	<form id="payment-form" method="post" action="<?= site_url() ?>/snap/finish">
 		<input type="hidden" name="result_type" id="result-type" value=""></div>
 		<input type="hidden" name="result_data" id="result-data" value=""></div>
 	</form>
@@ -29,7 +29,7 @@
 			$(this).attr("disabled", "disabled");
 
 			$.ajax({
-				url: 'https://tokohp.herokuapp.com//snap/token',
+				url: '<?= site_url() ?>/snap/token',
 				cache: false,
 
 				success: function (data) {
