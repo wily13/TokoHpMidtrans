c558e246bb4d74b972374864b14a6fd096a07730
Wily13 <wily.355@gmail.com>
2021-03-01 09:06:59 +0700

    add fuction redirect_ssl in hook.php

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+  function redirect_ssl() {
+    $CI =& get_instance();
+    $class = $CI->router->fetch_class();
+    $exclude =  array(' '); // memasukkan nama controller yang tidak diset SSL
+    if(!in_array($class,$exclude)) {
+      // redirect ke SSL (HTTP ke HTTPS)
+      $CI->config->config['base_url'] = str_replace('http://', 'https://', $CI->config->config['base_url']);
+      if ($_SERVER['SERVER_PORT'] != 443) redirect($CI->uri->uri_string());
+    } else {
+      // redirect tanpa SSL (HTTPS ke HTTP)
+      $CI->config->config['base_url'] = str_replace('https://', 'http://', $CI->config->config['base_url']);
+      if ($_SERVER['SERVER_PORT'] == 443) redirect($CI->uri->uri_string());
+    }
+  }
+  
+?>
