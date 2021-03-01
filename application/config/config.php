3a1a7dcbaecddeafe575917242c669e4e76869d5
Wily13 <wily.355@gmail.com>
2021-03-01 09:41:00 +0700

    edit config base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,16 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = "https://tokohp.herokuapp.com/";
+$config['base_url'] = "https://tokohp.herokuapp.com/";
 
 // $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 // $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
 // $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-
-$root	 = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER['HTTP_HOST'];
-$root	.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-
-$config['base_url'] = $root;
 
 
 /*
