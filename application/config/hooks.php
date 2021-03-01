8cc5c2d68f70691d18c5a56f3eb6147f917c78cc
Wily13 <wily.355@gmail.com>
2021-03-01 09:08:05 +0700

    edit file hook.php

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -12,19 +12,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-  function redirect_ssl() {
-    $CI =& get_instance();
-    $class = $CI->router->fetch_class();
-    $exclude =  array(' '); // memasukkan nama controller yang tidak diset SSL
-    if(!in_array($class,$exclude)) {
-      // redirect ke SSL (HTTP ke HTTPS)
-      $CI->config->config['base_url'] = str_replace('http://', 'https://', $CI->config->config['base_url']);
-      if ($_SERVER['SERVER_PORT'] != 443) redirect($CI->uri->uri_string());
-    } else {
-      // redirect tanpa SSL (HTTPS ke HTTP)
-      $CI->config->config['base_url'] = str_replace('https://', 'http://', $CI->config->config['base_url']);
-      if ($_SERVER['SERVER_PORT'] == 443) redirect($CI->uri->uri_string());
-    }
-  }
-  
-?>
+$hook['post_controller_constructor'][] = array(
+    'function' => 'redirect_ssl',
+    'filename' => 'ssl.php',
+    'filepath' => 'hooks'
+);
